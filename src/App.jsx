f56b13e628897b8b8e3d6d6b05341a4f41f3826f
alanychan <amy_cct@yahoo.com>
2023-02-26 17:02:12 +0800

    Typo.

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/login", element: <LoginPage /> },
-      { path: "/project/:id", element: <ProjectPage /> },
+      { path: "/projects/:id", element: <ProjectPage /> },
       { path: "/admin", element: <AdminPage /> },
     ],
   },
